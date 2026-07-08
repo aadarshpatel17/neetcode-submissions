d2202a0ae32e7bdc9950f88af002c65d3d3dfe8a
Aadarsh Patel <71113410+aadarshpatel17@users.noreply.github.com>
2026-07-08 09:10:31 +0530

    Bulk sync: 8 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int[] ch = new int[26];
+
+        for(int i=0; i<s.length(); i++) {
+            ch[s.charAt(i) - 'a']++;
+            ch[t.charAt(i) - 'a']--;
+        }
+
+        for(int i=0; i<26; i++) {
+            if(ch[i] != 0)
+                return false;
+        }
+
+        return true;
+
+        // Map<Character, Integer> mapS = new HashMap<>();
+        // Map<Character, Integer> mapT = new HashMap<>();
+        
+        // for(char c: s.toCharArray()) {
+        //     mapS.put(c, mapS.getOrDefault(c, 0) + 1);
+        // }
+        // for(char c: t.toCharArray()) {
+        //     mapT.put(c, mapT.getOrDefault(c, 0) + 1);
+        // }
+        // return mapS.equals(mapT);
+    }
+}
